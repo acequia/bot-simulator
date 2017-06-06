002529f91c7ca2a98af83e87d482183509cedab4
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2017-06-06 14:05:50 -0300

    Subscribe and publish to our topics

--- mqtt/publish.rb
+++ mqtt/publish.rb
@@ -1,7 +1,10 @@
 require 'mqtt'
 require_relative '../utils/credentials'
 
-topic = ARGV[0] || 'test_topic'
+virtual_host = ''
+user_id = ARGV[0] || 1703051
+
+routing_key_base = [virtual_host, 'bots', user_id].join '/'
 
 # Publish example
 MQTT::Client.connect(Credentials.host,
@@ -10,10 +13,14 @@ MQTT::Client.connect(Credentials.host,
 ) do |c|
   puts 'Publishing...'
 
-  # Fake measure
-  message = rand(50)
+  %w{temperature humidity io reset_reason}.each do |topic|
+    full_topic = [routing_key_base, topic].join '/'
 
-  c.publish(topic, message)
+    # Fake measure
+    message = rand(50)
 
-  puts "Sent #{message} to #{topic}"
+    c.publish(full_topic, message)
+
+    puts "Sent #{message} to #{full_topic}"
+  end
 end
--- mqtt/subscribe.rb
+++ mqtt/subscribe.rb
@@ -1,14 +1,15 @@
 require 'mqtt'
 require_relative '../utils/credentials'
 
-topic = ARGV[0] || 'test_topic'
+user_id = ARGV[0] || 1703051
+topic = "bots/#{user_id}/#"
 
 # Subscribe example
 MQTT::Client.connect(Credentials.host,
   username: Credentials.username,
   password: Credentials.password
 ) do |c|
-  puts 'Waiting for messages. To exit press CTRL+C'
+  puts "Waiting for messages on #{topic}. To exit press CTRL+C"
   c.get(topic) do |topic, message|
     puts "#{topic}: #{message}"
   end
